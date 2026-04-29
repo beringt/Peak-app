b53dec92ae676a7ed5cd09ce7ae9b57e6bfca3cb
beringt <thomasbering@hotmail.com>
2026-04-29 12:01:29 +0200

    Initialize App.tsx with Expo configuration

---
+++ App.tsx
@@ -0,0 +1,15 @@
+{
+  "expo": {
+    "name": "Peak-app",
+    "slug": "Peak-app",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "userInterfaceStyle": "light",
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "edgeToEdgeEnabled": true
+    }
+  }
+}
